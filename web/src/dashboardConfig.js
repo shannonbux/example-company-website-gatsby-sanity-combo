2dc5164ab1808791e682e91f42466ee7f0646eb7
shannonbux <shannon@gatsbyjs.com>
2019-06-03 11:14:45 -0700

    adding extension

---
+++ web/src/dashboardConfig.js
@@ -0,0 +1,14 @@
+export default {
+  widgets: [
+    {
+      name: 'gatsby',
+      options: {
+        instances: [
+          {
+            instanceUrl: 'https://example-company-website-gatsby-sanity-combo-2283414733.gtsb.io/'
+          }
+        ]
+      }
+    }
+  ]
+}
